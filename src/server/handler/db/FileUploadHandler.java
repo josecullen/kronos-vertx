66c5683934eea37e5d28cb4e9a573dd036fa98ef
josecullen <jose_cullen@hotmail.com>
2015-09-24 21:51:10 -0300

    pruebas de multiples uploads

--- src/server/handler/db/FileUploadHandler.java
+++ src/server/handler/db/FileUploadHandler.java
@@ -1,5 +1,8 @@
 package server.handler.db;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.Imagen;
 import service.AcontecimientoService;
 import service.ImagenService;
@@ -14,29 +17,28 @@ public class FileUploadHandler implements Handler<RoutingContext> {
 	String titulo;
 	ImagenService imagenService = new ImagenService(Globals.em);
 	AcontecimientoService acontecimientoService = new AcontecimientoService(Globals.em);
-	Imagen imagen;
+	List<Imagen> imagenes = new ArrayList<Imagen>();
 	@Override
 	public void handle(RoutingContext routingContext) {
 		HttpServerRequest req = routingContext.request();
 		req.setExpectMultipart(true);
 		req.endHandler(a->{
 			MultiMap formAttributes = req.formAttributes();
-      		System.out.println("endHandler formAttributes "+ formAttributes);
+      		System.out.println("endHandler formAttributes: "+ formAttributes);
+
       		titulo = formAttributes.get("titulo");
-            System.out.println("imagenService "+ imagenService.getMax());
-//            imagen =  imagenService.getLast();
-            System.out.println("última imagen : "+imagen.getTitulo());
             Globals.em.getTransaction().begin();
-            imagen.setTitulo(titulo);
-            Globals.em.merge(imagen);
+            imagenes.forEach(imagen -> {
+            	imagen.setTitulo(titulo);
+            	Globals.em.merge(imagen);	
+            });            
             Globals.em.getTransaction().commit();
-      		System.out.println("titulo "+titulo);
 		});
 		
         req.uploadHandler(new Handler<HttpServerFileUpload>() {
           @Override
           public void handle(final HttpServerFileUpload upload) {
-        	  System.out.println("uploadHandler imagenService "+ imagenService.getMax());
+        	  System.out.println("uploadHandler");
               long max = imagenService.getMax();
 
               upload.exceptionHandler(new Handler<Throwable>() {
@@ -49,14 +51,16 @@ public class FileUploadHandler implements Handler<RoutingContext> {
 	              @Override
 	              public void handle(Void event) {        
 	            		System.out.println("endHandler 2");	
-	            		System.out.println(max);
+	            		if(req.isEnded()){
+	            			System.out.println("isEnded");
+	            			req.response().end("Upload successful, you should see the file in the server directory");
+	            		}
 	            		
-	            		req.response().end("Upload successful, you should see the file in the server directory");
 	              }
               });
               Globals.em.getTransaction().begin();
-              imagen = imagenService.create("last");
-              
+              Imagen imagen = imagenService.create("last");
+              imagenes.add(imagen);
               Globals.em.getTransaction().commit();       
               upload.streamToFileSystem(""+imagen.getId());
 
